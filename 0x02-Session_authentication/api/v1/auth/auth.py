8f7e491473e20da96249e1e5c6f07df06e052aa3
Frankie Vexx <franblog7@gmail.com>
2023-08-11 11:27:26 +0300

    fixing errors

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
-"""Authentication module for the provided API.
+"""Authentication module for the API.
 """
+import os
 import re
 from typing import List, TypeVar
 from flask import request
 
 
 class Auth:
-    """Auth class.
+    """Authentication class.
     """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Checks if a path requires authentication.
@@ -36,3 +37,10 @@ class Auth:
         """Gets the current user from the request.
         """
         return None
+
+    def session_cookie(self, request=None) -> str:
+        """Gets the value of the cookie named SESSION_NAME.
+        """
+        if request is not None:
+            cookie_name = os.getenv('SESSION_NAME')
+            return request.cookies.get(cookie_name)
